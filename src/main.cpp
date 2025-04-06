904074d6e047dc5f780638a07c3392b66d5b5eb7
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2025-04-06 13:06:01 -0700

    Cleaned up code to make it more readable

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include <Arduino.h>
-#include <PID_v1.h>
 #include <VescUart.h>
-
-float current = 0.0;
-float previousCurrent = 0.0;
-float lerpSpeed = 0.1;
 
 // Pin to read Analog input from linear potentiometer
 int readPIN = A0;
@@ -14,11 +9,6 @@ int reversePIN = 3;
 
 /** Initiate VescUart class */
 VescUart UART;
-
-double SetPoint, Input, Output;
-
-double Kp=0.01, Ki=0.0, Kd=0.0;
-PID pid(&Input, &Output, &SetPoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
 
@@ -37,42 +27,25 @@ void setup() {
   pinMode(reversePIN, INPUT);
 
   delay(1000);
-
-  // UART.setDuty(0.2);
-
-  pid.SetMode(AUTOMATIC);
 }
 
 void loop() {
 
-  // float targetCurret = 5.0;
-
-  // current = lerp(previousCurrent, targetCurret, lerpSpeed);
-  // previousCurrent = current;
-
-  // UART.setCurrent(0.0);
-
+  /** Read the input from the button to reverse the current KAY probably add the shifter code here */
   bool reverse = digitalRead(reversePIN);
   
-  float current = analogRead(readPIN);
-  current = (current/1023) * 1.0;
-  // current = ((current - 512) / 512) * 2
-
-  SetPoint = current;
-
-  pid.Compute();
+  /** Read The Input from the petentiometer and map the values from 0 - 1*/
+  float duty = analogRead(readPIN);
+  duty = (duty/1023) * 1.0;
 
   if (reverse)
-    UART.setDuty(Output);
+    UART.setDuty(duty);
   else
-    UART.setDuty(-Output);
+    UART.setDuty(-duty);
 
 
   /** Call the function getVescValues() to acquire data from VESC */
   if ( UART.getVescValues() ) {
-    Serial.print("PID Output: ");
-    Serial.print(Output);
-    Serial.print(", ");
     Serial.print("RPM: ");
     Serial.print(UART.data.rpm);
     Serial.print(", ");
@@ -82,18 +55,8 @@ void loop() {
     Serial.print("Current:  ");
     Serial.print(UART.data.avgInputCurrent);
     Serial.print(", ");
-    Serial.print("Target Current: ");
-    Serial.print(current);
-    Serial.print(", ");
     Serial.print("Power:    ");
     Serial.println(UART.data.inpVoltage * UART.data.avgInputCurrent);
-
-    // Serial.print("Amp Hours: ");
-    // Serial.println(UART.data.ampHours);
-    // Serial.print("Tachometer: ");
-    // Serial.println(UART.data.tachometerAbs);
-
-    Input = UART.data.dutyCycleNow;
   }
   else
   {
@@ -103,6 +66,7 @@ void loop() {
   delay(500);
 }
 
+/** Lerp fuction to smoothly interpolate between two values */
 double lerp(double start, double end, double t)
 {
   return (1.0 - t) * start + t * end;
