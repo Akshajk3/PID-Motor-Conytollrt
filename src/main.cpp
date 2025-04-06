4397197a41b5f9a012561b814252be8618f112c7
kay singhal <130114612+iamkay556@users.noreply.github.com>
2025-04-06 13:25:44 -0700

    integrated limit switches and changing current accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 #include <VescUart.h>
+#include <ezButton.h>
+
+// init the switches -------> limit switches put them wherever there is space and add pin #s
+ezButton driveSwitch(2);
+ezButton neutralSwitch(3);
+ezButton reverseSwitch(4);
+
+enum GearMode { DRIVE, NEUTRAL, REVERSE };
+GearMode currentMode = NEUTRAL;
 
 // Pin to read Analog input from linear potentiometer
 int readPIN = A0;
-
-// Pin to read reverse direction from button
-int reversePIN = 3;
 
 /** Initiate VescUart class */
 VescUart UART;
@@ -23,26 +29,58 @@ void setup() {
   /** Define which ports to use as UART */
   UART.setSerialPort(&Serial1);
 
+  // if switches arent working, uncomment next 3 lines: ----> (Switch troubleshooting)
+  // pinMode(driveSwitch, INPUT_PULLUP);
+  // pinMode(nuetralSwitch, INPUT_PULLUP);
+  // pinMode(reverseSwitch, INPUT_PULLUP);
+
   pinMode(readPIN, INPUT);
   pinMode(reversePIN, INPUT);
+
+  driveSwitch.setDebounceTime(50);
+  nuetralSwitch.setDebounceTime(50);
+  reverseSwitch.setDebounceTime(50);
 
   delay(1000);
 }
 
 void loop() {
-
-  /** Read the input from the button to reverse the current KAY probably add the shifter code here */
-  bool reverse = digitalRead(reversePIN);
+  driveSwitch.loop();
+  neutralSwitch.loop();
+  reverseSwitch.loop();
   
   /** Read The Input from the petentiometer and map the values from 0 - 1*/
   float duty = analogRead(readPIN);
   duty = (duty/1023) * 1.0;
 
-  if (reverse)
-    UART.setDuty(duty);
-  else
-    UART.setDuty(-duty);
+  if (driveSwitch.wasPressed()) {
+    currentMode = DRIVE;
+    Serial.println("Mode swithched to : DRIVE");
+  }
+  if (neutralSwitch.wasPressed()) {
+    currentMode = NEUTRAL;
+    Serial.println("Mode swithched to : NUETRAL");
+  }
+  if (reverseSwitch.wasPressed()) {
+    currentMode = REVERSE;
+    Serial.println("Mode swithched to : REVERSE");
+  }
 
+  Serial.println("CURRENT MODE IS:" + currentMode);
+
+  switch (currentMode) {
+  case DRIVE:
+    UART.setDuty(duty);
+    break;
+
+  case REVERSE:
+    UART.setDuty(-duty);
+    break;
+
+  case NEUTRAL:
+    UART.setDuty(0);
+    break;
+  }
 
   /** Call the function getVescValues() to acquire data from VESC */
   if ( UART.getVescValues() ) {
@@ -62,6 +100,8 @@ void loop() {
   {
     Serial.println("Failed to get data!");
   }
+
+  
 
   delay(500);
 }
