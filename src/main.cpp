f5576902a64939f7a2f569892736205b7d501dfb
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2025-04-29 20:58:40 -0700

    added serial communication with a python script

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,320 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <SD.h>
+#include <SPI.h>
 #include <VescUart.h>
+#include <BuckPSU.h>
+#include <ezButton.h>
+#include <yxml.h>
+// #include <SoftwareWire.h>
+// #include <hd44780.h>
+// #include <hd44780ioClass/hd44780_I2Cexp.h>  // For I2C (PCF8574)
 
-// Pin to read Analog input from linear potentiometer
-int readPIN = A0;
 
-// Pin to read reverse direction from button
-int reversePIN = 3;
+// init the switches -------> limit switches put them wherever there is space and add pin #s
+ezButton driveSwitch(49);
+ezButton neutralSwitch(53);
+ezButton reverseSwitch(51);
 
-/** Initiate VescUart class */
+String mode = "none";
+String powerSource = "Off";
+
+#define readPIN A0       // pot pin
+#define SignalBattery 48 // signal from buck to activate battery relay
+#define SignalSolar 50   // signal from buck to activate solar relay
+
+#define relayBattery 24 // output pin to control battery relay
+#define relaySolar 25   // output pin to control solar relay
+
 VescUart UART;
+BuckPSU psu(Serial2);
 
-void setup() {
+File xmlFile;
 
-  /** Setup Serial port to display data */
+// SoftwareWire softI2C(3, 2); //sda scl
+
+LiquidCrystal_I2C lcd(0x26, 20, 4);
+LiquidCrystal_I2C lcd2(0x27, 20, 4);
+
+void setup()
+{
+
+  lcd.init();
+  lcd.backlight();
+
   Serial.begin(9600);
 
   /** Setup UART port (Serial1 on Atmega32u4) */
   Serial1.begin(115200);
-  
-  while (!Serial) {;}
+  Serial2.begin(4800);
+
+  // while (!Serial)
+  // {
+  //   ;
+  // }
 
   /** Define which ports to use as UART */
   UART.setSerialPort(&Serial1);
 
+
+  // if switches arent working, uncomment next 3 lines: ----> (Switch troubleshooting)
+  // pinMode(driveSwitch, INPUT_PULLUP);
+  // pinMode(neutralSwitch, INPUT_PULLUP);
+  // pinMode(reverseSwitch, INPUT_PULLUP);
+
   pinMode(readPIN, INPUT);
-  pinMode(reversePIN, INPUT);
+
+  pinMode(SignalBattery, INPUT_PULLUP);
+  pinMode(SignalSolar, INPUT_PULLUP);
+
+  pinMode(relayBattery, OUTPUT);
+  pinMode(relaySolar, OUTPUT);
+
+  // rekays start in off mode
+  digitalWrite(relayBattery, LOW);
+  digitalWrite(relaySolar, LOW);
+
+  driveSwitch.setDebounceTime(10);
+  neutralSwitch.setDebounceTime(10);
+  reverseSwitch.setDebounceTime(10);
+
+  lcd.clear();
+
+  // if (!SD.begin(53)) {
+  //   Serial.println("SD Card Init Failed");
+  //   return;
+  // }
+  // Serial.println("SD Card ready!");
 
   delay(1000);
 }
 
-void loop() {
+void dutyset(float start, float end)
+{
+  const float step = 0.005;
+  const float tolerance = 0.03;
 
-  /** Read the input from the button to reverse the current KAY probably add the shifter code here */
-  bool reverse = digitalRead(reversePIN);
-  
-  /** Read The Input from the petentiometer and map the values from 0 - 1*/
-  float duty = analogRead(readPIN);
-  duty = (duty/1023) * 1.0;
+  start = abs(start);
+  while (abs(start - end) > tolerance)
+  {
+    if (end > start)
+    {
+      start += step;
+      if (start > end)
+        start = end;
+    }
+    else if (end < start)
+    {
+      start -= step;
+      if (start < end)
+        start = end;
+    }
 
-  if (reverse)
-    UART.setDuty(duty);
-  else
-    UART.setDuty(-duty);
+    if (mode == "DRIVE")
+      UART.setDuty(start);
+    else if (mode == "REVERSE")
+      UART.setDuty(-start);
+    else if (mode == "NEUTRAL")
+      UART.setDuty(-start);
+    else
+    {}
+      // Serial.println("dutyset() func not working b/c no mode");
+
+    // delay(10);
+  }
+}
+
+void buckset(float start, float end)
+{
+  const float step = 1000;
+  const float tolerance = 500;
+
+  while (abs(start - end) > tolerance)
+  {
+    if (end > start)
+    {
+      start += step;
+      if (start > end)
+        start = end;
+    }
+    else if (end < start)
+    {
+      start -= step;
+      if (start < end)
+        start = end;
+    }
+
+    psu.setVoltageMilliVolts(start);
+
+    delay(10);
+  }
+}
 
 
-  /** Call the function getVescValues() to acquire data from VESC */
-  if ( UART.getVescValues() ) {
-    Serial.print("RPM: ");
-    Serial.print(UART.data.rpm);
-    Serial.print(", ");
-    Serial.print("Voltage:  ");
-    Serial.print(UART.data.inpVoltage);
-    Serial.print(", ");
-    Serial.print("Current:  ");
-    Serial.print(UART.data.avgInputCurrent);
-    Serial.print(", ");
-    Serial.print("Power:    ");
-    Serial.println(UART.data.inpVoltage * UART.data.avgInputCurrent);
+void loop()
+{ // --------------------------------- LOOP ---------------------------------
+
+  // lcd.clear();
+
+  int batonoff = digitalRead(SignalBattery);
+  // int batonoff = 0;
+  int solonoff = digitalRead(SignalSolar);
+
+  if (batonoff == 0)
+  {
+    digitalWrite(relayBattery, HIGH);
+    Serial.println("   BATTERY ON");
+    powerSource = "BATTERY ON";
   }
   else
   {
+    digitalWrite(relayBattery, LOW);
+    Serial.println("BATTERY OFF");
+    powerSource = "BATTERY OFF";
+  }
+  if (solonoff == 0)
+  {
+    digitalWrite(relaySolar, HIGH);
+    Serial.println("SOLAR ON");
+    powerSource = "SOLAR ON";
+    
+    buckset(0, 28000);
+    psu.enableOutput(true);
+  }
+  else
+  {
+    digitalWrite(relaySolar, LOW);
+    // Serial.println("SOLAR OFF");
+
+    
+    buckset(28000, 0);
+    psu.enableOutput(false);
+  }
+
+  driveSwitch.loop();
+  neutralSwitch.loop();
+  reverseSwitch.loop();
+
+  int revst = reverseSwitch.getState();
+  int neust = neutralSwitch.getState();
+  int drist = driveSwitch.getState();
+
+  /** Read The Input from the petentiometer and map the values from 0 - 1*/
+  float pot = analogRead(readPIN);
+  pot = (pot / 1023) * 1.0;
+  Serial.print("Pot: ");
+  Serial.print(pot);
+  Serial.print(", ");
+
+  if (drist == 0)
+  {
+    mode = "DRIVE";
+  }
+  if (neust == 0)
+  {
+    mode = "NEUTRAL";
+  }
+  if (revst == 0)
+  {
+    mode = "REVERSE";
+  }
+  Serial.print("Current Mode: ");
+  Serial.print(mode);
+  Serial.print(", ");
+
+  float rpm = 0.0;
+  float volt = 0.0;
+  float amps = 0.0;
+  float power = 0.0;
+  float duty = 0.0;
+
+  lcd.clear();
+  // lcd.print("Hi");
+
+  // call this function if u wanna get data
+  if (UART.getVescValues())
+  {
+
+    rpm = UART.data.rpm;
+    volt = UART.data.inpVoltage;
+    amps = UART.data.avgInputCurrent;
+    power = UART.data.inpVoltage * UART.data.avgInputCurrent;
+    duty = UART.data.dutyCycleNow;
+
+    // Serial.print(" DUTY CURRENT: ");
+    // Serial.print(duty);
+    // Serial.print("data given  on lcd");
+
+    String voltage  = "R: " + String((int)rpm);
+    String current = " C: " + String(amps);
+    String pow = "P: " + String((int)power);
+    String dut = " D: " + String(duty);
+    String driveMode = "Mode: " + mode;
+    String pSource = "Source: " + powerSource;
+
+    Serial.print(rpm); Serial.print(",");
+    Serial.print(volt); Serial.print(",");
+    Serial.print(amps); Serial.print(",");
+    Serial.print(power); Serial.print(",");
+    Serial.print(duty); Serial.print(",");
+    Serial.print(mode); Serial.print(",");
+    Serial.println(powerSource);    
+
+    lcd.setCursor(0, 0);
+    lcd.print(voltage);
+    lcd.setCursor(6, 0);
+    lcd.print(current);
+    lcd.setCursor(0, 1);
+    lcd.print(pow);
+    lcd.setCursor(6, 1);
+    lcd.print(dut);
+    lcd2.setCursor(0, 0);
+    lcd2.print(driveMode);
+    lcd2.setCursor(0, 1);
+    lcd2.print(pSource);
+
+    // String xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    // xmlData += "<sensor_data>\n";
+    // xmlData += "  <voltage>" + String(volt) + "</voltage>\n";
+    // xmlData += "  <current>" + String(amps) + "</current>\n";
+    // xmlData += "  <power>" + String(power) + "</power>\n";
+    // xmlData += "  <duty>" + String(duty) + "</duty>\n";
+    // xmlData += "  <timestamp>" + String(millis()) + "</timestamp>\n";
+    // xmlData += "</sensor_data>\n";
+
+    // File dataFile = SD.open("datalog.xml", FILE_WRITE);
+    // if (xmlFile) {
+    //   xmlFile.println(xmlData);
+    //   xmlFile.close();
+    // }
+    // else {
+    //   Serial.println("Error writing to xml file");
+    // }
+  }
+  else
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("Failed to get data");
     Serial.println("Failed to get data!");
   }
 
-  delay(500);
-}
+  if (mode == "DRIVE")
+  {
+    dutyset(duty, pot);
+  }
+  else if (mode == "REVERSE")
+  {
+    dutyset(duty, pot);
+  }
+  else if (mode == "NEUTRAL")
+  {
+    dutyset(duty, 0);
+  }
+  else
+  {
+    UART.setDuty(0);
+    // Serial.println("idk what im doing i didnt get a mode");
+  }
 
-/** Lerp fuction to smoothly interpolate between two values */
-double lerp(double start, double end, double t)
-{
-  return (1.0 - t) * start + t * end;
+  delay(10);
 }
